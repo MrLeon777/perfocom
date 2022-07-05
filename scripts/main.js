9749c13f21721b994ce88ce448fac4cabc793509
unknown <valentin7gubin@gmail.com>
2022-07-05 11:11:35 +0300

    1

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const news = new Swiper('.news-slider', {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  // width: 500,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
